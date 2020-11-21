a027fbe05367eae094bd61f27f9dc8abe47caf01
Daniel Garcia <lequodo@gmail.com>
2020-11-21 04:02:00 +0100

    Modificar nombre de base de datos

--- sql/Conexion.php
+++ sql/Conexion.php
@@ -2,7 +2,7 @@
 define("HOST", "localhost");
 define("USERNAME", "root");
 define("PASSWORD", "");
-define("DATABASE", "tiendas_moviles");
+define("DATABASE", "tienda_moviles");
 function getConexionPDO() {
     $opciones = array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
     return new PDO(
