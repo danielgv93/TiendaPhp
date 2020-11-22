7313d000eff87b4ebad1b643a5693e6356b720de
Daniel Garcia <lequodo@gmail.com>
2020-11-22 02:18:24 +0100

    Crear elementos basicos de la pagina. Crear updateStock y addMovil

--- borrar_insertar.php
+++ borrar_insertar.php
@@ -1,5 +1,25 @@
 <?php
 require_once "sql/queries.php";
+
+$stockUpdated = "";
+$movilAdded = "";
+if (isset($_POST["updateStock"])) {
+    try {
+        updateStock($_POST["idModelo"], $_POST["stockDispositivo"]);
+        $stockUpdated = "El stock se ha modificado a " . $_POST["stockDispositivo"] . " unidades";
+    } catch (Exception $e) {
+        $stockUpdated = $e->getMessage();
+    }
+}
+if (isset($_POST["insertarMovil"])) {
+    try {
+        addMovil($_POST["modelo"], $_POST["precio"], $_POST["gama"], $_POST["anio"], $_POST["ram"], $_POST["almacenamiento"],
+            $_POST["procesador"], $_POST["bateria"], $_POST["pulgadas"], $_POST["camara"], $_POST["notch"]);
+        $movilAdded = "Movil añadido con éxito";
+    } catch (Exception $e) {
+        $movilAdded = $e->getMessage();
+    }
+}
 ?>
 
 <!doctype html>
@@ -12,22 +32,93 @@ require_once "sql/queries.php";
     <title>Borrar-Insertar</title>
 </head>
 <body>
-<!--TODO: CREAR FUNCION PARA UPDATEAR-->
-Modificar
-<form action="<?= $_SERVER["PHP_SELF"] ?>" method="post">
-    <select>
-        <?php foreach (getDispositivos() as $dispositivo) : ?>
-            <option value="<?= $dispositivo["id"] ?>"><?= $dispositivo["modelo"] ?></option>
+<a href="perfil.php"><input type="button" value="Volver"></a>
+<!-- ELEGIR MODELO PARA CAMBIAR EL STOCK -->
+<h2>Modificar</h2>
+<form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+    <select name="modelo">
+        <?php
+        $dispositivos = getDispositivos();
+        foreach ($dispositivos as $id => $dispositivo) : ?>
+            <option value="<?= $id ?>" <?php if (isset($_POST["elegirModelo"]) && ($id == $_POST["modelo"])) echo "selected" ?>><?= $dispositivo["modelo"] ?></option>
         <?php endforeach; ?>
     </select>
-    <input type="number" name="stockDispositivo">
-    <input type="submit" name="update" value="Actualizar Stock">
+    <input type="submit" name="elegirModelo" value="Elegir Modelo">
 </form>
-Insertar
-<form action="<?= $_SERVER["PHP_SELF"] ?>" method="post">
-    <input type="text" name="Modelo">
-    <input type="number" name="precio" step="any">
-    <!--TODO: SEGUIR CON LOS INPUTS-->
+<!-- CAMBIAR STOCK -->
+<?php if (isset($_POST["elegirModelo"])): ?>
+    <h3>Modelo elegido <?= $dispositivos[$_POST["modelo"]]["modelo"] ?></h3>
+    <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+        <input type="hidden" name="idModelo" value="<?= $_POST["modelo"] ?>">
+        <input type="number" name="stockDispositivo" value="<?= $dispositivos[$_POST["modelo"]]["stock"] ?>">
+        <input type="submit" name="updateStock" value="Modificar Stock">
+    </form>
+<?php endif; ?>
+
+<?= $stockUpdated ?>
+<!-- ELEGIR DISPOSITIVO PARA INSERTAR -->
+<!-- HABRÁ 2 FORMS EN FUNCIÓN DE LA ELECCIÓN -->
+<h2>Insertar</h2>
+<form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+    <select name="tipoDispositivo">
+        <option value="movil"<?php if (isset($_POST["submitTipoDisp"]) && ($_POST["tipoDispositivo"] == "movil")) echo "selected" ?>>
+            Móvil
+        </option>
+        <option value="reloj"<?php if (isset($_POST["submitTipoDisp"]) && ($_POST["tipoDispositivo"] == "reloj")) echo "selected" ?>>
+            Reloj
+        </option>
+    </select>
+    <input type="submit" name="submitTipoDisp" value="Elegir">
 </form>
+<?= $movilAdded ?>
+<!-- INSERTAR MOVIL A LA BASE DE DATOS -->
+<?php if (isset($_POST["submitTipoDisp"]) && $_POST["tipoDispositivo"] == "movil"): ?>
+    <h2>Insertar Características del Móvil</h2>
+    <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+        Modelo<input type="text" name="modelo">
+        Precio<input type="number" name="precio" step="any">
+        Gama<select name="gama">
+            <option value="alta">Alta</option>
+            <option value="media">Media</option>
+            <option value="baja">Baja</option>
+        </select>
+        Año de lanzamiento<input type="number" name="anio">
+        RAM<input type="text" name="ram">
+        Almacenamiento<input type="number" name="almacenamiento">
+        Procesador<input type="text" name="procesador">
+        Batería<input type="number" name="bateria">
+        Pulgadas pantalla<input type="number" name="pulgadas" step="any">
+        Cámara <input type="number" name="camara" step="any">
+        Notch<select name="notch">
+            <option value="1">Si</option>
+            <option value="0">No</option>
+        </select>
+        <input type="submit" name="insertarMovil" value="Insertar Móvil">
+    </form>
+<?php endif; ?>
+<!-- INSERTAR RELOJ A LA BASE DE DATOS -->
+<?php if (isset($_POST["submitTipoDisp"]) && $_POST["tipoDispositivo"] == "reloj"): ?>
+    <h2>Insertar Características del Reloj</h2>
+    <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="post">
+        Modelo<input type="text" name="modelo">
+        Precio<input type="number" name="precio" step="any">
+        Gama<select name="gama">
+            <option value="alta">Alta</option>
+            <option value="media">Media</option>
+            <option value="baja">Baja</option>
+        </select>
+        Año de lanzamiento<input type="number" name="anio">
+        RAM<input type="text" name="ram">
+        Almacenamiento<input type="number" name="almacenamiento">
+        Procesador<input type="text" name="procesador">
+        Batería<input type="number" name="bateria">
+        Pulgadas pantalla<input type="number" name="pulgadas" step="any">
+        SIM<select name="sim">
+            <option value="1">Si</option>
+            <option value="0">No</option>
+        </select>
+        <input type="submit" name="insertarReloj" value="Insertar Reloj">
+    </form>
+<?php endif; ?>
 </body>
 </html>
