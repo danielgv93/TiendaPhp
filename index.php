b14862b57371513db46b5ff2dc49dca5e9e1c0de
Joseba Mantecon <josebamsmsa@gmail.com>
2020-11-26 23:54:11 +0100

    login y registro hechos ya, falta enlazar bien el script

--- index.php
+++ index.php
@@ -43,150 +43,109 @@ if (isset($_POST["registro"])) {
     }
 }
 ?>
-
 <!DOCTYPE html>
-<html lang="es">
+<html>
 
 <head>
-    <meta charset="UTF-8">
+    <title>Registro</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Index</title>
-    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
-    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-    <link rel="stylesheet" href="css/estilo.css">
-    <script src="js/script.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" integrity="sha512-1PKOgIY59xJ8Co8+NE6FZ+LOAZKjy+KY8iq0G4B3CyeY6wYHN3yt9PW0XpSriVlkMXe40PTKnXrLnZ9+fkDaog==" crossorigin="anonymous" />
+    <link rel="stylesheet" href="style.css">
+    <link rel="shorcut icon" href="img/iconTitle.png">
 </head>
 
 <body>
     <div class="container">
-        <div class="row">
-            <div class="col-md-4">
-                <div class="card border-danger mb-3" style="width:32rem;">
-                    <img class="card-img-top" src="img/logo3.jpg" alt="Card image cap">
-                    <div class="card-body text-primary">
-                        <h4 class="card-title">Conócenos</h4>
-                        <p class="card-text">Tu tienda de informática online, y cada vez la de más gente. Precios imbatibles y calidad superior!</p>
-                        <a href="#" class="btn btn-primary">Visítanos</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-md-offset-2">
-                <div class="panel panel-login">
-                    <div class="panel-heading">
-                        <div class="row">
-                            <div class="col-xs-6">
-                                <a href="#" class="active" id="login-form-link">Iniciar sesión</a>
-                            </div>
-                            <div class="col-xs-6">
-                                <a href="#" id="register-form-link">Regístrate ahora</a>
-                            </div>
-                        </div>
-                        <hr>
-                    </div>
-                    <div class="panel-body">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <form id="login-form" action="<?= $_SERVER["PHP_SELF"] ?>" method="post" role="form" style="display: block;">
+        <div class="registro">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="izquierda">
+                        <form class="formulario text-center" id="register-form" action="<?= $_SERVER["PHP_SELF"] ?>" method="post" role="form">
+                            <header class="ml-4">Crear nueva cuenta</header>
 
-                                    <div class="form-group">
-                                        <input type="text" name="usuario" id="usuario" tabindex="1" class="form-control" placeholder="Usuario" value="<?php if ((isset($_POST["registro"]) || isset($_POST["login"])) && isset($_POST["usuario"])) echo htmlEntities($_POST["usuario"], ENT_QUOTES); ?>">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="password" name="contraseña" id="contraseña" tabindex="2" class="form-control" placeholder="Contraseña">
-                                    </div>
-                                    <div class="form-group text-center">
-                                        <input type="checkbox" tabindex="3" class="" name="recordar" id="recordar">
-                                        <label for="recordar"> Recordarme</label>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="row">
-                                            <div class="col-sm-6 col-sm-offset-3">
-                                                <input type="submit" name="login" id="login" tabindex="4" class="form-control btn btn-login" value="Iniciar sesión">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="row">
-                                            <div class="col-lg-12">
-                                                <div class="text-center">
-                                                    <a href="https://www.google.com/search?q=como+recuperar+contrase%C3%B1as+no+guardadas+en+google+chrome&rlz=1C1CHBF_esES857ES857&oq=como+&aqs=chrome.1.69i57j0i67j0i433j0l2j69i65j69i60l2.2247j0j4&sourceid=chrome&ie=UTF-8" target="blank" tabindex="5" class="olvido-contraseña">¿Has olvidado
-                                                        tu contraseña?</a>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </form>
-                                <form id="register-form" action="<?= $_SERVER["PHP_SELF"] ?>" method="post" role="form" style="display: none;">
-                                    <div class="form-group">
-                                        <input type="text" name="nombre" id="usuario" tabindex="1" class="form-control" placeholder="Nombre" required value="<?php if (isset($_POST["registro"]) && isset($_POST["nombre"])) echo htmlEntities($_POST["nombre"], ENT_QUOTES); ?>">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="text" name="apellidos" id="apellidos" tabindex="1" class="form-control" placeholder="Apellidos" required value="<?php if (isset($_POST["registro"]) && isset($_POST["apellidos"])) echo htmlEntities($_POST["apellidos"], ENT_QUOTES); ?>">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="text" name="usuario" id="usuario" tabindex="1" class="form-control" placeholder="Usuario" required value="<?php if (isset($_POST["registro"]) && isset($_POST["usuario"])) echo htmlEntities($_POST["usuario"], ENT_QUOTES); ?>">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="email" name="email" id="email" tabindex="1" class="form-control" placeholder="Correo electronico" required value="<?php if (isset($_POST["registro"]) && isset($_POST["email"])) echo htmlEntities($_POST["email"], ENT_QUOTES); ?>">
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="password" name="contraseña" id="contraseña" tabindex="2" class="form-control" placeholder="Contraseña" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <input type="password" name="confirm-contraseña" id="confirm-contraseña" tabindex="2" class="form-control" placeholder="Confirmar contraseña" required>
-                                    </div>
-                                    <div class="form-group">
-                                        <div class="row">
-                                            <div class="col-sm-6 col-sm-offset-3">
-                                                <input type="submit" name="registro" id="registro" tabindex="4" class="form-control btn btn-register mb-2" value="Crear cuenta">
-                                            </div>
-                                        </div>
-                                    </div>
-                                    <?php if (!empty($errorLogin)) : ?>
-                                        <div class='alert alert-danger mt-3 col-3 offset-5' role='alert'>
-                                            <?= $errorLogin ?>
-                                        </div>
-                                    <?php endif; ?>
-                                    <?php if (!empty($correctAddUser)) : ?>
-                                        <div class='alert alert-success mt-2 col-3 offset-5' role='alert'>
-                                            <?= $correctAddUser ?>
-                                        </div>
-                                    <?php endif; ?>
-                                    <?php if (!empty($errorAddUser)) : ?>
-                                        <div class='alert alert-danger mt-2 col-3 offset-5' role='alert'>
-                                            <?= $errorAddUser ?>
-                                        </div>
-                                    <?php endif; ?>
-                                    <?php if (!empty($passRepetida)) : ?>
-                                        <div class='alert alert-danger mt-3 col-3 offset-5' role='alert'>
-                                            <?= $passRepetida ?>
-                                        </div>
-                                    <?php endif; ?>
-                                    <?php if (!empty($camposSinIntroducir)) : ?>
-                                        <div class='alert alert-danger mt-3 col-3 offset-5' role='alert'>
-                                            <?= $camposSinIntroducir ?>
-                                        </div>
-                                    <?php endif; ?>
-                                </form>
+                            <div class="form-group">
+                                <i class="fas fa-signature"></i>
+                                <input class="caja" type="text" name="nombre" id="nombre" placeholder="Nombre" required value="<?php if (isset($_POST["registro"]) && isset($_POST["nombre"])) echo htmlEntities($_POST["nombre"], ENT_QUOTES); ?>">
                             </div>
+
+                            <div class="form-group">
+                                <i class="fas fa-user"></i>
+                                <input class="caja" type="text" name="usuario" id="usuario" required placeholder="Usuario" value="<?php if ((isset($_POST["registro"]) || isset($_POST["login"])) && isset($_POST["usuario"])) echo htmlEntities($_POST["usuario"], ENT_QUOTES); ?>">
+                            </div>
+
+                            <div class="form-group">
+                                <i class="fas fa-envelope"></i>
+                                <input class="caja" type="text" id="email" placeholder="Email" required value="<?php if (isset($_POST["registro"]) && isset($_POST["email"])) echo htmlEntities($_POST["email"], ENT_QUOTES); ?>">
+                            </div>
+
+                            <div class="form-group">
+                                <i class="fas fa-lock"></i>
+                                <input class="caja" type="password" id="contraseña" placeholder="Contraseña" required>
+                            </div>
+
+                            <div class="form-group">
+                                <i class="fas fa-check-circle"></i>
+                                <input class="caja" type="password" id="contraseña2" placeholder="Confimar contraseña" required>
+                            </div>
+
+                            <div class="form-group">
+                                <label>
+                                    <input id="check_1" name="check_1" type="checkbox" required><small> He leído y acepto la política de privacidad</small></input>
+                                    <div class="invalid-feedback">Debes rellenar todos los campos</div>
+                                </label>
+                            </div>
+
+                            <input type="submit" class="boton" value="CREAR CUENTA ">
+
+                        </form>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="derecha">
+                        <div class="info">
+                            <header><img class="img-fluid ml-5" src="img/logo.png" alt="Logo corporativo"></header>
+                            <p>Tu tienda de informática online, y cada día la de más gente!
+                                Calidad superior a precios imbatibles
+                            </p>
                         </div>
+
                     </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="container-fluid">
-        <div class="row align-items-end" id="pie">
-            <div class="col-12 bg-primary text-white text-center" id="pie" style="position: absolute;  bottom: 0; width: 100%;">
-                <footer>
-                    <p>&trade; TODOS LOS DERECHOS RESERVADOS</p>
-                    <p><a href="http://web2.iesmiguelherrero.com/" class="alert-link" target="blank" style="color:white;">IES Miguel Herrero</a> </p>
-                    <p>P3 &copy; 2020</p>
-                </footer>
-            </div>
-        </div>
-    </div>
+
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
 </body>
 
 </html>
+
+<?php if (!empty($errorLogin)) : ?>
+    <div class='alert alert-danger mt-3 col-3 offset-5' role='alert'>
+        <?= $errorLogin ?>
+    </div>
+<?php endif; ?>
+<?php if (!empty($correctAddUser)) : ?>
+    <div class='alert alert-success mt-2 col-3 offset-5' role='alert'>
+        <?= $correctAddUser ?>
+    </div>
+<?php endif; ?>
+<?php if (!empty($errorAddUser)) : ?>
+    <div class='alert alert-danger mt-2 col-3 offset-5' role='alert'>
+        <?= $errorAddUser ?>
+    </div>
+<?php endif; ?>
+<?php if (!empty($passRepetida)) : ?>
+    <div class='alert alert-danger mt-3 col-3 offset-5' role='alert'>
+        <?= $passRepetida ?>
+    </div>
+<?php endif; ?>
+<?php if (!empty($camposSinIntroducir)) : ?>
+    <div class='alert alert-danger mt-3 col-3 offset-5' role='alert'>
+        <?= $camposSinIntroducir ?>
+    </div>
+<?php endif; ?>
---
+++ registro.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Inicio de sesión</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=Poppins:600&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/a81368914c.js"></script>
+    <link rel="stylesheet" href="registro.css">
+    <link rel="shorcut icon" href="img/iconTitle.png">
+    <script src="js/script.js"></script>
+</head>
+
+<body>
+    <div class="container">
+        <div class="img">
+            <img src="img/logo.png">
+        </div>
+        <div class="login-content">
+            <form action="index.html">
+                <img src="https://img.icons8.com/cotton/64/000000/user-male-circle.png" />
+                <h2 class="title">Bienvenido</h2>
+                <div class="input-div one">
+                    <div class="i">
+                        <i class="fas fa-user"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Usuario</h5>
+                        <input type="text" class="form-control">
+                    </div>
+                </div>
+                <div class="input-div pass">
+                    <div class="i">
+                        <i class="fas fa-lock"></i>
+                    </div>
+                    <div class="div">
+                        <h5>Contraseña</h5>
+                        <input type="password" class="form-control">
+                    </div>
+                </div>
+                <a href="https://www.google.com/search?q=como+recuperar+contrase%C3%B1as+no+guardadas+en+google+chrome&rlz=1C1CHBF_esES857ES857&oq=como+rec&aqs=chrome.1.69i57j69i59j0l6.1949j0j4&sourceid=chrome&ie=UTF-8">He olvidado mi contraseña</a>
+                <input type="submit" class="boton" value="ENTRAR">
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
